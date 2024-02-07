32c1121bf7dc9c80d8c10879f5adf6ef728be36e
Kerim Hudson <kh@Kerims-MacBook-Pro.local>
2024-02-07 12:06:25 +0000

    add reading for sequencer

--- index.js
+++ index.js
@@ -1,51 +1,56 @@
-import { Hono} from 'hono'
-import { basicAuth } from 'hono/basic-auth'
-import { logger } from 'hono/logger'
-import { cors } from 'hono/cors'
-import { serve } from '@hono/node-server'
+import { Hono } from "hono";
+import { basicAuth } from "hono/basic-auth";
+import { logger } from "hono/logger";
+import { cors } from "hono/cors";
+import { serve } from "@hono/node-server";
 import postgres from "postgres";
 
-const pg = postgres(process.env.POSTGRES_CONNECTION_STRING)
+const pg = postgres(process.env.POSTGRES_CONNECTION_STRING);
 
+const app = new Hono();
+app.use("*", logger());
+app.use("*", cors("*"));
 
-const app = new Hono()
-app.use('*', logger())
-app.use('*', cors('*'))
-app.use('*', basicAuth({
-    username: process.env.BASIC_USERNAME,
-    password: process.env.BASIC_PASSWORD
-}))
+app.get("/health", async (c) => {
+  return c.json({ ok: true }, 200);
+});
 
-app.get('/health', async (c) => {
-
-    
-    return c.json({ ok: true}, 200)
-
-})
-
-app.post('/readings', async (c) => {
-
-    const { readings, ...rest } = await c.req.json()
-    console.log(readings, rest)
+app
+  .use(
+    basicAuth({
+      username: process.env.BASIC_USERNAME,
+      password: process.env.BASIC_PASSWORD,
+    })
+  )
+  .post("/readings", async (c) => {
+    const { readings, ...rest } = await c.req.json();
+    console.log(readings, rest);
     const [data] = await pg`insert into aq_readings ${pg({
-        timestamp: rest.timestamp,
-        uid: rest.uid,
-        nickname: rest.nickname,
-        model: rest.model,
-        temperature: readings.temperature,
-        humidity: readings.humidity,
-        pressure: readings.pressure,
-        noise: readings.noise,
-        pm1: readings.pm1,
-        pm2_5: readings.pm2_5,
-        pm10: readings.pm10
+      timestamp: rest.timestamp,
+      uid: rest.uid,
+      nickname: rest.nickname,
+      model: rest.model,
+      temperature: readings.temperature,
+      humidity: readings.humidity,
+      pressure: readings.pressure,
+      noise: readings.noise,
+      pm1: readings.pm1,
+      pm2_5: readings.pm2_5,
+      pm10: readings.pm10,
     })} returning *`;
 
+    return c.json(data, 201);
+  });
 
-    return c.json(data, 201)
-})
+app.get("/readings/sequencer", (c) => {
+  await[
+    data
+  ] = pg`select * from aq_readings where nickname = 'aq-game' ORDER BY timestamp DESC LIMIT 1`;
 
-export default app
+  return c.json(data, 200);
+});
+
+export default app;
 serve({ fetch: app.fetch, port: process.env.PORT }, (info) => {
-    console.log(`Listening on http://${info.address}:${info.port}`)
-})
+  console.log(`Listening on http://${info.address}:${info.port}`);
+});
